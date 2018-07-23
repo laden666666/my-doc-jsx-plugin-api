6252c452cd776fb2b4ae560246384bd980705434
laden666666 <njz198869@gmail.com>
2018-07-24 06:00:52 +0800

    提交浏览器兼容控件

--- src/HTMLRender/blockTags/BrowserList.ts
+++ src/HTMLRender/blockTags/BrowserList.ts
@@ -4,30 +4,36 @@
 import {BlockNode, HTMLRender, jsxStr2Nodes} from "my-doc-jsx"
 declare function require(name: string);
 
-class browserList extends BlockNode<HTMLRender>{
+class BrowserList extends BlockNode<HTMLRender>{
     constructor(node){
         super(node)
         this.priority = 0;
     }
+    
+    async renderTd(versionInfo: string | boolean){
+        return `<td align="center" style="background-color: #60d848">${( versionInfo == true || versionInfo == 'true') ? '√' : versionInfo }</td>`
+    }
+    
+    async renderTh(browserName: string){
+        return `<th align="center">${browserName}</td>`
+    }
+
     async render(render: HTMLRender){
         const browserList = Object.keys(this.props)
         
         browserList.filter(browser => this.props[browser] != null)
         
-        return render.renderBlockNodes(jsxStr2Nodes(`<table>
-            <tr>
-            ${
-                browserList.map(browser=>`<th align="center">${ browser }</th>`).join('')
-            }
-            </tr>
-            <tr>
-            ${
-                browserList.map(browser=>`<td align="center">${ this.props[browser] === true ? '√' : this.props[browser] }</td>`).join('')
-            }
-            </tr>
-        </table>`, render, null))
+        return `<table cellspacing="1" style="margin: 0 auto;font-size: 14px;background-color: #f9f9f9;color: #036;padding: 3px;border-radius: 4px;border: 1px solid rgba(220, 220, 220, .5);">
+    <colgroup width="100" span="5" align="center"></colgroup>
+    <tr style="height: 30px;">
+        ${browserList.map(browser=>this.renderTh(browser)).join('')}
+    </tr>
+    <tr style="color: #000;line-height: 28px;font-weight: bold;">
+        ${browserList.map(browser=>this.renderTd( this.props[browser])).join('')}
+    </tr>
+</table>`
     }
 }
 
-export default browserList;
+export default BrowserList;
 
