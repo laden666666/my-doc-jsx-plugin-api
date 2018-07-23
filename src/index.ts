9062eba165b86fb73087dbf178a81c111eb13dbb
laden666666 <njz198869@gmail.com>
2018-07-24 06:13:14 +0800

    提交对部分功能的修改

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ import htmlAPI from './HTMLRender/blockTags/API'
 import markdownAPI from './MarkdownRender/blockTags/API'
 import htmlHTMLExample from './HTMLRender/blockTags/HTMLExample'
 import markdownHTMLExample from './MarkdownRender/blockTags/HTMLExample'
-import htmlbrowserList from './HTMLRender/blockTags/browserList'
-import markdownbrowserList from './MarkdownRender/blockTags/browserList'
+import htmlBrowserList from './HTMLRender/blockTags/browserList'
+import markdownBrowserList from './MarkdownRender/blockTags/browserList'
 import htmlNpmInfo from './HTMLRender/blockTags/NpmInfo'
 import markdownNpmInfo from './MarkdownRender/blockTags/NpmInfo'
+import htmlTitle from './HTMLRender/blockTags/Title'
+import markdownTitle from './MarkdownRender/blockTags/Title'
 
 //当前版本
 //通过DefinePlugin对去package.json的版本
@@ -16,12 +18,14 @@ class Plugin extends BasePlugin{
         super()
         this.registerBlockNode('HTML', 'api', htmlAPI)
         this.registerBlockNode('HTML', 'html-example', htmlHTMLExample)
-        this.registerBlockNode('HTML', 'browser-list', htmlbrowserList)
+        this.registerBlockNode('HTML', 'browser-list', htmlBrowserList)
         this.registerBlockNode('HTML', 'npm-info', htmlNpmInfo)
+        this.registerBlockNode('HTML', 'title', htmlTitle)
         this.registerBlockNode('MARKDOWN', 'api', markdownAPI)
         this.registerBlockNode('MARKDOWN', 'html-example', markdownHTMLExample)
-        this.registerBlockNode('MARKDOWN', 'browser-list', markdownbrowserList)
+        this.registerBlockNode('MARKDOWN', 'browser-list', markdownBrowserList)
         this.registerBlockNode('MARKDOWN', 'npm-info', markdownNpmInfo)
+        this.registerBlockNode('MARKDOWN', 'title', markdownTitle)
     }
 
     static version: string = PLUGIN_VERSION
